608a7af6f4c9dd8de9e19f15df1586fab3d625fb
malchak <michaelmalchak@gmail.com>
2012-04-28 14:04:35 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeamProject::Application.config.secret_token = '97e90d4009486d583b9ffe8f779119e9f9aece8c67cb91738bbf648cc1b0040bf91044853b975d4c19e73b33622745781bd653ab01f9a17fe442d29695d33849'
