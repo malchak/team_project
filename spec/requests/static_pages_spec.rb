259e1a4232997d79df5171bab1e3aaa685097464
malchak <michaelmalchak@gmail.com>
2012-04-28 19:08:21 -0700

    Initital static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  
+  describe "Home page" do
+    it "should have the content 'The Hive" do
+      visit '/static_pages/home'
+      page.should have_content('The Hive')
+    end
+  end
+  
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_content('Help')
+    end
+  end
+  
+  describe "About page" do
+    it "should have the content 'About'" do
+      visit '/static_pages/about'
+      page.should have_content('About')
+    end
+  end
+  
+    
+end
+
